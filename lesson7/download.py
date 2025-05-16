175de4fd65321dbe877474e0648cc78717f05529
Lianna1618 <liannnaqa@gmail.com>
2025-05-16 17:03:32 +0400

    lesson 7

--- lesson7/download.py
+++ lesson7/download.py
@@ -1,17 +1,29 @@
+import os
 import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.page_load_strategy = "normal"
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument("--window-size=1440,900")
+download_dir = os.path.join(os.getcwd(), "img_files")
 
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {"download.default_directory": download_dir}
+
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.page_load_strategy = "normal"
+chrome_options.add_argument("--window-size=1440,900")
+chrome_options.add_argument("--disable-extensions")
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
+
 driver.get("https://the-internet.herokuapp.com/download")
 
-driver.find_element()
+time.sleep(2)
+
+driver.find_element("xpath", "//a[text()='webdriverIO.png']").click()
+
+time.sleep(5)
--- lesson7/upload.py
+++ lesson7/upload.py
@@ -1,4 +1,6 @@
+import os
 import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -13,4 +15,14 @@ service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
-driver.get("https://expired.badssl.com/")
+driver.get("https://the-internet.herokuapp.com/upload")
+
+time.sleep(5)
+
+driver.find_element(
+    "xpath", "//*[@id='file-upload']").send_keys(f"{os.getcwd()}/img_files/webdriverIO.png"
+)
+
+time.sleep(5)
+ 
+# "//input[type='file']"
